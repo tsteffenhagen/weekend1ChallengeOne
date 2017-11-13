752c9f608b9170391ca6e129a34e207ed90e8913
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2017-11-13 10:29:11 -0600

    went through calculations

--- script.js
+++ script.js
@@ -22,4 +22,23 @@ function addEmployeeButtonClick() {
     //push the new object into an array
     employees.push( newEmployee );
     console.log(employees);
+
+    calculateSalaries();
 } // end addEmployeeButtonClick
+
+
+function calculateSalaries(){
+    console.log('in calculateSalaries');
+    var totalSalaries = 0;
+    //loop through employees array
+    for (let i = 0; i < employees.length; i++) {
+        //convert each salary to a number
+        console.log( Number( employees[i].salary ) );        
+        //add salary to total salaries 
+        totalSalaries += Number( employees[i].salary );
+    }// end for each employee
+    console.log('Total Salaries:', totalSalaries);
+    var monthlySalaryCost = totalSalaries / 12;
+    console.log('Monthly Salary Cost:', monthlySalaryCost);
+    
+};//end calculateSalaries
