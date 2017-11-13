067a54c8eacf9082a6fedbe97614090298fdc12f
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2017-11-13 09:56:59 -0600

    created and pushed into global array

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready( readyNow );
+var employees = [];
 
 function readyNow() {
     // on click event handler for the add employee button
@@ -7,4 +8,18 @@ function readyNow() {
 
 function addEmployeeButtonClick() {
     console.log('in addEmployeeButtonClick');
+    //get user input
+   
+    //put input into an object
+    var newEmployee = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        employeeId: $('#employeeIdIn').val(),
+        jobDescription: $('#jobDescriptionIn').val(),
+        salary: $('#salaryIn').val()
+    }// end NewEmployee object
+    console.log('adding', newEmployee);
+    //push the new object into an array
+    employees.push( newEmployee );
+    console.log(employees);
 } // end addEmployeeButtonClick
